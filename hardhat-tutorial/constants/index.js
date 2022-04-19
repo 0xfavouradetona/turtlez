4b30b1f9073e2c614a11baf7e9667ce8e154adf7
Favour Adetona <96779581+0xfavouradetona@users.noreply.github.com>
2022-04-19 10:52:44 +0100

    final

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = " 0xD3adA04326dEDb0C449f7548cB76347f066e9d87";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
